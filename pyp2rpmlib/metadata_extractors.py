2f3cace845d53f8a433e855dd20098419d4426ed
Bohuslav Kabrda <bkabrda@redhat.com>
2012-05-11 13:07:47 +0200

    Some renaming and stuff (ok, these are completely crazy commits, I know)

--- pyp2rpmlib/metadata_extractors.py
+++ pyp2rpmlib/metadata_extractors.py
@@ -3,18 +3,17 @@ import xmlrpclib
 from pyp2rpmlib.package_data import PackageData
 
 class MetadataExtractor(object):
-    def __init__(self, pkg_data_obj):
-        self.pkg_data_obj = pkg_data_obj
-
-    def do_extraction(self):
+    def extract_data(self):
         raise NotImplementedError('Whoops, do_extraction method not implemented by %s.', % self.class)
 
 class PypiMetadataExtractor(MetadataExtractor):
     def __init__(self, pkg_data_obj, client, name, version):
-        super(MetadataExtractor, self).__init__(pkg_data_obj)
         self.client = client
         self.name = name
         self.version = version
 
-    def do_extraction(self):
-        
+    def extract_data(self):
+        release_urls = self.client.release_urls(name, version)
+
+class LocalMetadataExtractor(MetadataExtractor):
+    pass
--- pyp2rpmlib/package_data.py
+++ pyp2rpmlib/package_data.py
@@ -1,7 +1,15 @@
 class PackageData(object):
-    def __init__(self, name, version, md5 = None, pypi_url = None, pkg_type = None, local_file = None):
+    def __init__(self, name, version):
         self.name = name
+        self.version = version
+
+class PypiData(PackageData):
+    def __init__(self, name, version, md5, url):
+        super(PackageData, self).__init__(name, version)
         self.md5 = md5
-        self.pypi_url = pypi_url
-        self.pkg_type = pkg_type
+        self.url = url
+
+class LocalData(PackageData):
+    def __init__(self, name, version, local_file):
+        super(PackageData, self).__init__(name, version)
         self.local_file = local_file
