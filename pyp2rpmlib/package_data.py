53241aa439eb2dc73020235c00d6c31d3a635e95
Bohuslav Kabrda <bkabrda@redhat.com>
2012-05-17 08:30:22 +0200

    Yet more comments

--- pyp2rpmlib/package_data.py
+++ pyp2rpmlib/package_data.py
@@ -4,6 +4,7 @@ import time
 from pyp2rpmlib import utils
 
 class PackageData(object):
+    """A simple object that carries data about a package."""
     def __init__(self, local_file, name, version):
         self.local_file = local_file
         self.name = name
@@ -29,11 +30,14 @@ class PackageData(object):
 
     @property
     def changelog_date_packager(self):
+        """Returns part of the changelog entry, containing date and packager.
+        """
         packager = subprocess.Popen('rpmdev-packager', stdout = subprocess.PIPE).communicate()[0].strip()
         date_str = time.strftime('%a %b %d %Y', time.gmtime())
         return "%s %s" % (date_str, packager)
 
 class PypiData(PackageData):
+    """Carries data about package from PyPI"""
     def __init__(self, local_file, name, version, md5, url):
         super(PypiData, self).__init__(local_file, name, version)
         self.md5 = md5
