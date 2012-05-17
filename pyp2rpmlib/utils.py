3cb062bb7596c0e36ad0d176e968a36593b80314
Bohuslav Kabrda <bkabrda@redhat.com>
2012-05-17 09:02:06 +0200

    Coooomeeeents

--- pyp2rpmlib/utils.py
+++ pyp2rpmlib/utils.py
@@ -1,6 +1,7 @@
 import functools
 
 def memoize_by_args(func):
+    """Memoizes return value of a func based on args."""
     memory = {}
 
     @functools.wraps(func)
@@ -14,6 +15,12 @@ def memoize_by_args(func):
     return memoized
 
 def rpm_name(name):
+    """Returns name of the package coverted to (possibly) correct package name according to Packaging Guidelines.
+    Args:
+        name: name to convert
+    Returns:
+        Converted name of the package, that should be in line with Fedora Packaging Guidelines.
+    """
     if name.lower().find('py') != -1:
         return name
     else:
