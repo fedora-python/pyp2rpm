30a8787bad0700f16234755517f5f28776e0cc7c
Bohuslav Kabrda <bkabrda@redhat.com>
2012-05-21 13:08:04 +0200

    Filter for macroing the package name with %{pypi_name}

--- pyp2rpmlib/filters.py
+++ pyp2rpmlib/filters.py
@@ -3,4 +3,10 @@ from pyp2rpmlib import utils
 def for_python_version(name, version):
     return utils.rpm_name(name, version)
 
-__all__ = [for_python_version]
+def macroed_pkg_name(name):
+    if name.startswith('python-'):
+        return 'python-%{pypi_name}'
+    else:
+        return '%{pypi_name}'
+
+__all__ = [for_python_version, macroed_pkg_name]
