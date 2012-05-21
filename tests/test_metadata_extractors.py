950f2bf2af8c8a8e3382dc42722622b12613f5ac
Bohuslav Kabrda <bkabrda@redhat.com>
2012-05-21 15:32:31 +0200

    Test pypi metadata extractor a bit

--- tests/test_metadata_extractors.py
+++ tests/test_metadata_extractors.py
@@ -1,4 +1,5 @@
 import os
+import xmlrpclib
 
 from tarfile import TarFile
 from zipfile import ZipFile
@@ -8,6 +9,7 @@ import pytest
 from flexmock import flexmock
 
 from pyp2rpmlib.metadata_extractors import *
+from pyp2rpmlib import settings
 
 tests_dir = os.path.split(os.path.abspath(__file__))[0]
 
@@ -89,3 +91,37 @@ class TestMetadataExtractor(object):
     ])
     def test_has_extension(self, i, expected):
         assert self.e[i].has_extension == expected
+
+
+class TestPypiMetadataExtractor(object):
+    td_dir = '%s/test_data/' % tests_dir
+    client = flexmock(
+        release_urls = lambda n, v: [{'md5_digest': '9a7a2f6943baba054cf1c28e05a9198e',
+                                      'url': 'http://pypi.python.org/packages/source/r/restsh/restsh-0.1.tar.gz'}],
+        release_data = lambda n, v: {'description': 'UNKNOWN',
+                                     'release_url': 'http://pypi.python.org/pypi/restsh/0.1',
+                                     'classifiers': ['Development Status :: 4 - Beta',
+                                                     'Intended Audience :: Developers',
+                                                     'License :: OSI Approved :: BSD License',
+                                                     'Operating System :: OS Independent'
+                                                     ],
+                                     'license': 'BSD',
+                                     'summary': 'A simple rest shell client',
+                                     'spam': 'eggs and beans'
+                                    }
+    )
+
+    def setup_method(self, method):
+        # we will only test getting stuff from the client => pass spam as file
+        self.e = PypiMetadataExtractor('spam', 'restsh', '0.1', self.client)
+
+    @pytest.mark.parametrize(('what', 'expected'), [
+        ('description', 'UNKNOWN'),
+        ('md5','9a7a2f6943baba054cf1c28e05a9198e'),
+        ('url', 'http://pypi.python.org/packages/source/r/restsh/restsh-0.1.tar.gz'),
+        ('license', 'BSD'),
+        ('summary', 'A simple rest shell client')
+    ])
+    def test_extract(self, what, expected):
+        data = self.e.extract_data()
+        assert getattr(data, what) == expected
