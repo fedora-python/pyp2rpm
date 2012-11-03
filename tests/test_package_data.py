49a1564c9587436ad6c36a42bb6caa1203179c8b
Slavek Kabrda <slavek.kabrda@gmail.com>
2012-11-03 16:04:11 +0100

    fix test_package_data

--- tests/test_package_data.py
+++ tests/test_package_data.py
@@ -4,7 +4,7 @@ import pytest
 
 from flexmock import flexmock
 
-from pyp2rpmlib.package_data import *
+from pyp2rpm.package_data import *
 
 class TestPackageData(object):
     @pytest.mark.parametrize(('s', 'expected'), [
@@ -12,12 +12,12 @@ class TestPackageData(object):
             ('Spam', 'Spam'),
     ])
     def test_summary_with_dot(self, s, expected):
-        pd = PackageData('spam', 'spam', 'spam')
+        pd = PackageData('spam', 'spam', 'python-spam', 'spam')
         pd.summary = s
         assert pd.summary == expected
 
     def test_get_nonexistent_attribute(self):
-        pd = PackageData('spam', 'spam', 'spam')
+        pd = PackageData('spam', 'spam', 'python-spam', 'spam')
         assert pd.eggs == 'TODO:'
 
     @pytest.mark.parametrize(('n', 'expected'), [
@@ -26,5 +26,5 @@ class TestPackageData(object):
         ('spam', 'spam'),
     ])
     def test_underscored_name(self, n, expected):
-        pd = PackageData('spam', n, 'spam')
+        pd = PackageData('spam', n, 'python-spam', 'spam')
         assert pd.underscored_name == expected
