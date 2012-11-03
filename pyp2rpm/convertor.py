a3850d462209eb5d4ac08d69174f25f7e09acab5
Slavek Kabrda <slavek.kabrda@gmail.com>
2012-11-03 13:29:26 +0100

    Introduce NameConvertor, allow specifying distro, according which rules to convert

--- pyp2rpm/convertor.py
+++ pyp2rpm/convertor.py
@@ -10,6 +10,7 @@ import jinja2
 from pyp2rpm import exceptions
 from pyp2rpm import filters
 from pyp2rpm import metadata_extractors
+from pyp2rpm import name_convertor
 from pyp2rpm import package_data
 from pyp2rpm import package_getters
 from pyp2rpm import settings
@@ -19,6 +20,7 @@ class Convertor(object):
     def __init__(self, name = None, version = None,
                  save_dir = settings.DEFAULT_PKG_SAVE_PATH,
                  template = settings.DEFAULT_TEMPLATE,
+                 distro = settings.DEFAULT_DISTRO,
                  source_from = settings.DEFAULT_PKG_SOURCE,
                  metadata_from = settings.DEFAULT_METADATA_SOURCE,
                  base_python_version = settings.DEFAULT_PYTHON_VERSION,
@@ -31,6 +33,7 @@ class Convertor(object):
         self.base_python_version = base_python_version
         self.python_versions = python_versions
         self.template = template
+        self.name_convertor = name_convertor.NameConvertor(distro)
         if not self.template.endswith('.spec'):
             self.template = '%s.spec' % self.template
 
@@ -46,14 +49,14 @@ class Convertor(object):
         """
         # move file into position
         try:
-            local_file = self.getter().get()
+            local_file = self.getter.get()
         except (exceptions.NoSuchPackageException, OSError) as e:
             sys.exit(e)
 
         # save name and version from the file (rewrite if set previously)
-        self.name, self.version = self.getter().get_name_version()
+        self.name, self.version = self.getter.get_name_version()
 
-        data = self.metadata_extractor(local_file).extract_data()
+        data = self.get_metadata_extractor(local_file).extract_data()
         data.base_python_version = self.base_python_version
         data.python_versions = self.python_versions
         jinja_env = jinja2.Environment(loader = jinja2.ChoiceLoader([
@@ -69,8 +72,9 @@ class Convertor(object):
         except jinja2.exceptions.TemplateNotFound: # absolute path not found => search in default template dir
             jinja_template = jinja_env.get_template(self.template)
 
-        return jinja_template.render(data = data)
+        return jinja_template.render(data = data, name_convertor = name_convertor)
 
+    @property
     def getter(self):
         """Returns the proper PackageGetter subclass.
         Returns:
@@ -82,24 +86,33 @@ class Convertor(object):
         if not self._getter:
             if self.source_from == 'pypi':
                 if self.name == None: raise exceptions.NameNotSpecifiedException('Must specify package when getting from PyPI.')
-                self._getter = package_getters.PypiDownloader(self.client, self.name, self.version, self.save_dir)
+                self._getter = package_getters.PypiDownloader(self.client,
+                                                              self.name,
+                                                              self.version,
+                                                              self.save_dir)
             elif os.path.exists(self.source_from):
-                self._getter = package_getters.LocalFileGetter(self.source_from, self.save_dir)
+                self._getter = package_getters.LocalFileGetter(self.source_from,
+                                                               self.save_dir)
             else:
                 raise exceptions.NoSuchSourceException('"%s" is neither one of preset sources nor a file.' % self.source_from)
 
         return self._getter
 
-    def metadata_extractor(self, local_file):
+    def get_metadata_extractor(self, local_file):
         """Returns the proper MetadataExtractor subclass.
         Returns:
             The proper MetadataExtractor subclass according to self.metadata_from.
         """
         if not self._metadata_extractor:
             if self.metadata_from == 'pypi':
-                self._metadata_extractor = metadata_extractors.PypiMetadataExtractor(local_file, self.name, self.version, self.client)
+                self._metadata_extractor = metadata_extractors.PypiMetadataExtractor(local_file,
+                                                                                     self.name,
+                                                                                     self.version,
+                                                                                     self.client)
             else:
-                self._metadata_extractor = metadata_extractors.LocalMetadataExtractor(local_file, self.name, self.version)
+                self._metadata_extractor = metadata_extractors.LocalMetadataExtractor(local_file,
+                                                                                      self.name,
+                                                                                      self.version)
 
         return self._metadata_extractor
 
--- pyp2rpm/utils.py
+++ pyp2rpm/utils.py
@@ -16,57 +16,6 @@ def memoize_by_args(func):
 
     return memoized
 
-def rpm_versioned_name(name, version):
-    """Properly versions the name.
-    For example:
-    rpm_versioned_name('python-foo', '26') will return python26-foo
-    rpm_versioned_name('pyfoo, '3') will return python3-pyfoo
-
-    If version is same as settings.DEFAULT_PYTHON_VERSION, no change is done.
-
-    Args:
-        name: name to version
-        version: version or None
-    Returns:
-        Versioned name or the original name if given version is None.
-    """
-    if version == settings.DEFAULT_PYTHON_VERSION:
-        return name
-
-    versioned_name = name
-    if version:
-        if name.startswith('python-'):
-            versioned_name = name.replace('python-', 'python%s-' % version)
-        else:
-            versioned_name = 'python%s-%s' % (version, name)
-
-    return versioned_name
-
-def rpm_name(name, python_version = None):
-    """Returns name of the package coverted to (possibly) correct package name according to Packaging Guidelines.
-    Args:
-        name: name to convert
-        python_version: python version for which to retrieve the name of the package
-    Returns:
-        Converted name of the package, that should be in line with Fedora Packaging Guidelines.
-        If for_python is not None, the returned name is in form python%(version)s-%(name)s
-    """
-    rpmized_name = name
-    if settings.DEFAULT_TEMPLATE =="mageia":
-        exclude_string = "python-"
-    else:
-        exclude_string = "py-"
-
-    if name.lower().find(exclude_string) == -1: # name doesn't contain "py" => prefix with "python-"
-        rpmized_name = 'python-%s' % name
-    elif name.endswith('-python'): # name ends with "-python" => strip that and put it to front
-        rpmized_name = 'python-%s' % name.replace('-python', '')
-    # else the name contains "py" as its part => do nothing
-    # or the name is in form "python-%(name)s", which is fine, toO
-    if settings.DEFAULT_TEMPLATE == "mageia":
-        rpmized_name = rpmized_name.lower()
-    return rpm_versioned_name(rpmized_name, python_version)
-
 def license_from_trove(trove):
     """Finds out license from list of trove classifiers.
     Args:
