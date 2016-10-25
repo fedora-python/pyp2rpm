3781e0424a4eb795a8927fe5febb3880203af796
Michal Cyprian <mcyprian@redhat.com>
2016-10-25 11:18:04 +0200

    Add spectest marker to spec file tests

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -22,6 +22,7 @@ class TestSpec(object):
         ('Jinja2', '-t epel6', 'python-Jinja2_epel6.spec'),
         ('buildkit', '-b2', 'python-buildkit.spec'),
     ])
+    @pytest.mark.spectest
     def test_spec(self, package, options, expected):
         with open(self.td_dir + expected) as fi:
             self.spec_content = fi.read()
