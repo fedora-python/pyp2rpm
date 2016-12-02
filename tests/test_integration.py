55feb49a441bf58cf7d31805fe238f24ff1f6027
Iryna Shcherbina <ishcherb@redhat.com>
2016-12-02 16:52:15 +0100

    Add a test for the rename feature
    - Add an integration test for python-sphinx.spec
    - Use set comparison to have a better visualized failure message

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -2,8 +2,6 @@ import pytest
 import os
 from scripttest import TestFileEnvironment
 
-from pyp2rpm.bin import main
-
 tests_dir = os.path.split(os.path.abspath(__file__))[0]
 
 
@@ -22,6 +20,7 @@ class TestSpec(object):
         ('Jinja2', '-t epel6', 'python-Jinja2_epel6.spec'),
         ('buildkit', '-b2', 'python-buildkit.spec'),
         ('StructArray', '-b2', 'python-StructArray.spec'),
+        ('Sphinx', '-r python-sphinx', 'python-sphinx.spec'),
     ])
     @pytest.mark.spectest
     def test_spec(self, package, options, expected):
@@ -29,7 +28,7 @@ class TestSpec(object):
             self.spec_content = fi.read()
         res = self.env.run('{0} {1} {2}'.format(self.exe, package, options))
         # changelog have to be cut from spec files
-        assert ''.join(res.stdout.split('\n')[1:-4]) == ''.join(self.spec_content.split('\n')[1:-4])
+        assert set(res.stdout.split('\n')[1:-4]) == set(self.spec_content.split('\n')[1:-4])
 
 
 class TestSrpm(object):
